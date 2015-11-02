e47efd0cca58bf3a902ab30f0c4e9aa939ef85ec
nikolavn <nikola.nenoff@gmail.com>
2015-11-02 09:32:17 +0200

    Added Homework for Web Services

---
+++ Web Services And Cloud/02.ASP.NET Web API/StudentsSystem/Data/StudentsSystem.Data/StudentsSystemDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace StudentsSystem.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    using Models;
+
+    public class StudentsSystemDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public StudentsSystemDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static StudentsSystemDbContext Create()
+        {
+            return new StudentsSystemDbContext();
+        }
+    }
+}
---
+++ Web Services And Cloud/02.ASP.NET Web API/StudentsSystem/Server/StudentsSystem.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StudentsSystem.Api.WebApiApplication" Language="C#" %>
