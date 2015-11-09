893564f7d1425559b30a37e3a4664093d9973c1e
nikolavn <nikola.nenoff@gmail.com>
2015-11-09 14:25:07 +0200

    Added Consuming Web Services homework

---
+++ Web Services And Cloud/04.ConsumingWebServices/ConsumingServicesJSONPlaceHolder/Post.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsumingServicesJSONPlaceHolder
+{
+    using Newtonsoft.Json;
+
+    public class Post
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("body")]
+        public string Body { get; set; }
+    }
+}
---
+++ Web Services And Cloud/04.ConsumingWebServices/ConsumingServicesJSONPlaceHolder/Startup.cs
@@ -0,0 +1,62 @@
+﻿namespace ConsumingServicesJSONPlaceHolder
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+
+    using Newtonsoft.Json;
+
+    class Startup
+    {
+        private const string DefaultUri = "http://jsonplaceholder.typicode.com/posts";
+
+        static void Main()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(DefaultUri);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            Console.WriteLine("Enter search phrase or enter for default (lor): ");
+            var phrase = Console.ReadLine();
+            if (string.IsNullOrEmpty(phrase))
+            {
+                phrase = "lor";
+            }
+
+            Console.WriteLine("Enter number of posts or enter for default(10): ");
+            int numberOfPosts;
+            if (!int.TryParse(Console.ReadLine(), out numberOfPosts))
+            {
+                numberOfPosts = 10;
+            }
+
+            var posts = GetPosts(client, phrase, numberOfPosts);
+
+            Console.WriteLine("{0} post(s) with title containing {1}: " + Environment.NewLine, numberOfPosts, phrase);
+
+            foreach (var post in posts)
+            {
+                Console.WriteLine("Title: {0}", post.Title);
+                Console.WriteLine("Content: {0}", post.Body);
+                Console.WriteLine();
+            }
+        }
+
+        private static ICollection<Post> GetPosts(HttpClient client, string postTitle, int postsCount)
+        {
+            HttpResponseMessage response = client.GetAsync("").Result;
+
+            var allPostsJson = response.Content.ReadAsStringAsync().Result;
+            var allPosts = JsonConvert.DeserializeObject<List<Post>>(allPostsJson);
+
+            var filtered = allPosts
+                .Where(p => p.Title.Contains(postTitle))
+                .Take(postsCount)
+                .ToList();
+
+            return filtered;
+        }
+    }
+}
