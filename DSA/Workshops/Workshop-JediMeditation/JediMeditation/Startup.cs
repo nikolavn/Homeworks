d2a347e41e88d6101e5ab735c4ce0e4cc3752bf6
nikolavn <nikola.nenoff@gmail.com>
2015-11-09 11:16:23 +0200

    Added more tasks to Recursion homework

---
+++ DSA/Workshops/Workshop-JediMeditation/JediMeditation/Startup.cs
@@ -0,0 +1,22 @@
+﻿namespace JediMeditation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Startup
+    {
+        static void Main()
+        {
+            var jedisWaiting = int.Parse(Console.ReadLine());
+
+            string[] jedisQueue = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var masters = jedisQueue.Where(x => x[0] == 'm').Select(x => x);
+            var knights = jedisQueue.Where(x => x[0] == 'k').Select(x => x);
+            var padawans = jedisQueue.Where(x => x[0] == 'p').Select(x => x);
+
+            Console.WriteLine("{0} {1} {2}",string.Join(" ", masters),string.Join(" ", knights),string.Join(" ", padawans));
+        }
+    }
+}
